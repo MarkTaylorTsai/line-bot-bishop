39b398d4cefdc886d671829bda0883331e3e6e31
marktaylortsai <marktaylortsai@gmail.com>
2025-09-04 22:13:14 +0800

    update error commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const lineConfig = {
 
 // Supabase configuration
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SERVICE_ROLE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Bishop configuration - who receives the reminders
